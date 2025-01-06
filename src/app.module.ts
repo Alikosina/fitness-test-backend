caa83dc816a3f0c7f469aa3ac91ec4184b81ebc3
Slo <slo@MacBook-Pro-Slo.local>
2025-01-06 14:19:01 +0300

    Integrated TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const dbConfig = require('../ormconfig.json'); 
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(dbConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
