18bca250ac632b730168705822779b9ee88b5a58
Slo <slo@MacBook-Pro-Slo.local>
2025-01-13 13:03:07 +0300

    Fixed jwt error after signIn

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,18 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { UsersService } from 'src/users/users.service';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from 'src/users/user.entity';
 
 @Module({
   imports: [
+    JwtModule.register({ secret: 'hard!to-guess_secret', global: true }),
     UsersModule,
     PassportModule,
-    TypeOrmModule.forFeature([Users])
+    TypeOrmModule.forFeature([Users]),
   ],
-  providers: [AuthService, UsersService, JwtService],
+  providers: [AuthService, UsersService],
   controllers: [AuthController],
 })
 export class AuthModule {}
